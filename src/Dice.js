330841c8c7eb1967e6957f95c08971d9cef9102f
Ben Cotton <ben@Bens-MacBook-Pro.local>
2023-01-13 16:30:12 +0000

    dice select (display only)

--- src/Dice.js
+++ src/Dice.js
@@ -1,19 +1,18 @@
 import { AiFillHeart } from 'react-icons/ai';
 import { FaPaw } from 'react-icons/fa';
+import { useState } from 'react';
 
 const Dice = ({id, dieValue}) => {
-    console.log(dieValue)
-    const selectDie = (id) => {
-        console.log('selected', id)
-    }
 
+    const [dieSelected, setDieSelected] = useState(false);
+ 
     const displayValue = (dieValue) => {
         if (dieValue === 4) dieValue = <AiFillHeart/>
 		if (dieValue === 5) dieValue = <FaPaw/>
         return dieValue;
     }
 
-    return <div className='dice' onClick={() => selectDie(id)}>
+    return <div className={`dice ${dieSelected ? 'die-selected' : ''}`} onClick={() => setDieSelected(!dieSelected)}>
         <p className='dice-number'>{displayValue(dieValue)}</p>
     </div>;
 }
