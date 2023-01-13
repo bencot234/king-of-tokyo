caae70e1ec73902061b80a101b3f88792d62be79
Ben Cotton <ben@Bens-MacBook-Pro.local>
2023-01-13 21:19:51 +0000

    reserve dice functionality

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import { useState } from 'react';
 
 function App() {
 	const [dice, setDice] = useState([
-		{id: 1, value: 1},
-		{id: 2, value: 1},
-		{id: 3, value: 1},
-		{id: 4, value: 1},
-		{id: 5, value: 1},
+		{id: 1, value: 1, selected: false},
+		{id: 2, value: 1, selected: false},
+		{id: 3, value: 1, selected: false},
+		{id: 4, value: 1, selected: false},
+		{id: 5, value: 1, selected: false},
+		{id: 6, value: 1, selected: false},
 	]);
 
 	const [players, setPlayers] = useState([
@@ -21,7 +22,7 @@ function App() {
 			health: 10,
 			inTokyo: false,
 			isTurn: true,
-    },
+		},
 		{
 			id: 2,
 			name: 'player 2',
@@ -29,14 +30,43 @@ function App() {
 			health: 10,
 			inTokyo: false,
 			isTurn: false,
-    }
+		}
 	]);
 
 	const rollDice = () => {
 		setDice(dice.map(die => {
 			let newValue = Math.floor(Math.random() * 5) + 1;
-			return {id: die.id, value: newValue};
+			if (!die.selected) {
+				return {...die, value: newValue};
+			}
+			return die;
 		}))
+	}
+
+	const handleSubmit = () => {
+		let pointsGained = 0;
+		let totalOnes = 0;
+		let totalTwos = 0;
+		let totalThrees = 0;
+	
+		dice.map(die => {
+			if (die.value === 1) totalOnes += 1;
+			if (die.value === 2) totalTwos += 1;
+			if (die.value === 3) totalThrees += 1;
+		})
+		if (totalOnes >= 3) pointsGained += (totalOnes - 2);
+		if (totalTwos >= 3) pointsGained += (totalTwos - 1);
+		if (totalThrees >= 3) pointsGained += totalThrees;
+		
+		setPlayers(
+			players.map((player) => {
+				if (player.id === 1)
+				{
+					return {...player, points: player.points + pointsGained};
+				}
+				return player;
+			})
+		);
 	}
 
 	return (
@@ -44,9 +74,16 @@ function App() {
 			<div className='dice-container'>
 				{dice.map(die => {
 					const {id, value} = die;
-					return <Dice key={id} id={id} dieValue={value}/>
+					return <Dice 
+						key={id} 
+						id={id} 
+						dieValue={value} 
+						dice={dice} 
+						setDice={setDice}
+					/>
 				})}
-				<button onClick={rollDice} className='roll-dice-btn'>Roll dice</button>
+				<button onClick={rollDice} className='btn roll-dice-btn'>Roll dice</button>
+				<button className='btn submit-btn' onClick={handleSubmit}>Submit</button>
 			</div>
 			<div className='players-container'>
 				{players.map(player => {
--- src/Dice.js
+++ src/Dice.js
@@ -1,10 +1,21 @@
 import { AiFillHeart } from 'react-icons/ai';
 import { FaPaw } from 'react-icons/fa';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-const Dice = ({id, dieValue}) => {
+const Dice = ({id, dieValue, dice, setDice}) => {
 
     const [dieSelected, setDieSelected] = useState(false);
+
+    useEffect(() => {
+        setDice(
+            dice.map(die => {
+                if (die.id === id) {
+                    return {...die, selected: dieSelected};
+                }
+                return die;
+            })
+        );
+    }, [dieSelected]);
  
     const displayValue = (dieValue) => {
         if (dieValue === 4) dieValue = <AiFillHeart/>
